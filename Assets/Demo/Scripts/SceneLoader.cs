cb1b1b4d1f30ce01a1476a92d61e2a5bf283dcf5
dingxiaowei <1213250243@qq.com>
2022-12-20 22:18:32 +0800

    添加测试场景

--- Assets/Demo/Scripts/SceneLoader.cs
+++ Assets/Demo/Scripts/SceneLoader.cs
@@ -2,24 +2,24 @@
 
 namespace libx
 {
-	public class SceneLoader : MonoBehaviour
-	{
-		public string scene; 
-		public float delay;
-		
-		// Use this for initialization
-		void Start () {
-			if (delay > 0)
-			{
-				Invoke("LoadScene", 3);		 
-				return;
-			}
-			LoadScene();
-		}
+    public class SceneLoader : MonoBehaviour
+    {
+        public string scene;
+        public float delay;
 
-		void LoadScene()
-		{
-			Assets.LoadSceneAsync(scene);
-		}
-	}
+        void Start()
+        {
+            if (delay > 0)
+            {
+                Invoke("LoadScene", 3);
+                return;
+            }
+            LoadScene();
+        }
+
+        void LoadScene()
+        {
+            Assets.LoadSceneAsync(scene);
+        }
+    }
 }
