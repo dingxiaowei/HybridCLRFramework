722f48a946012fceaceb2246c64065aabca4b060
dingxiaowei <1213250243@qq.com>
2023-02-14 11:38:32 +0800

    按钮按下

--- Assets/Demo/Scripts/XR/BallController.cs
+++ Assets/Demo/Scripts/XR/BallController.cs
@@ -7,6 +7,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class BallController : MonoBehaviour
 {
     public XRController rightController;
+    public float speed = 2.5f;
 
     void Update()
     {
@@ -15,7 +16,33 @@ public class BallController : MonoBehaviour
         if (success)
         {
             var position = transform.position;
-            transform.position = new Vector3(position.x - result.x * Time.deltaTime, position.y, position.z + result.y * Time.deltaTime);
+            transform.position = new Vector3(position.x + speed * result.x * Time.deltaTime, position.y, position.z + speed * result.y * Time.deltaTime);
+        }
+
+        //侧边的扳机键的值
+        rightController.inputDevice.TryGetFeatureValue(CommonUsages.grip, out float floatValue);
+        if (floatValue > 0.8f)
+        {
+            Debug.LogError($"握把键按下大于0.8f  实际值是多少:{floatValue}");
+        }
+
+        InputHelpers.IsPressed(rightController.inputDevice, InputHelpers.Button.Grip, out bool isPressed, 0.8f);
+        if (isPressed)
+        {
+            Debug.LogError($"第二种方法按下去大于0.8f");
+        }
+
+        //按钮按键
+        bool isDown; //记录是否按下主按钮
+        if (rightController.inputDevice.TryGetFeatureValue(CommonUsages.primaryButton, out isDown) && isDown)
+        {
+            Debug.Log("按键按下了主键A");
+        }
+
+        bool isBDown; //记录是否按下了副按钮
+        if (rightController.inputDevice.TryGetFeatureValue(CommonUsages.secondaryButton, out isBDown) && isBDown)
+        {
+            Debug.Log("按键按下了副键B");
         }
     }
 }
