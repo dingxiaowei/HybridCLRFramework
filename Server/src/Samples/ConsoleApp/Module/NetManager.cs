da2e78de07bdcf930500d96dc0a1363c0707b4cf
dingxiaowei <1213250243@qq.com>
2023-02-28 23:49:24 +0800

    1

--- Server/src/Samples/ConsoleApp/Module/NetManager.cs
+++ Server/src/Samples/ConsoleApp/Module/NetManager.cs
@@ -9,9 +9,7 @@ namespace ServerDemo
 {
     class NetManager : Singleton<NetManager>
     {
-        List<IWebSocketConnection> allSockets = new List<IWebSocketConnection>();
-        Dictionary<int, IWebSocketConnection> idSocketMap = new Dictionary<int, IWebSocketConnection>();
-        Dictionary<IWebSocketConnection, int> socketIdMap = new Dictionary<IWebSocketConnection, int>();
+        Dictionary<int, Session> sessionMap = new Dictionary<int, Session>();
         WebSocketServer server = null;
         int socketIndex = 0;
 
@@ -20,31 +18,53 @@ namespace ServerDemo
             server = new WebSocketServer("ws://127.0.0.1:8081");
             server.Start(socket =>
             {
-                var id = socket.ConnectionInfo.Id;
-                socket.OnOpen = () =>
+                ++socketIndex;
+                Session session = new Session(socketIndex, socket, (s) =>
                 {
-                    allSockets.Add(socket);
-                    Console.WriteLine(id + ": Connected");
-                    socketIndex++;
-                    socketIdMap.Add(socket, socketIndex);
-                    idSocketMap.Add(socketIndex, socket);
-                };
-                socket.OnClose = () =>
+                    sessionMap.Add(socketIndex, s);
+                }, (s) =>
                 {
-                    idSocketMap.Remove(socketIdMap[socket]);
-                    allSockets.Remove(socket);
-                    Console.WriteLine(id + ": Closed");
-                    socketIdMap.Remove(socket);
-                };
-                socket.OnBinary += BroacastBinaryMsg;
-                socket.OnMessage += BroacastStringMsg;
-                socket.OnError = error =>
-                {
-                    Console.WriteLine(socket.ConnectionInfo.Id + ": " + error.Message);
-                };
+                    sessionMap.Remove(socketIndex);
+                });
             });
         }
 
+        public void BraocastBinaryMsg(byte[] bytes)
+        {
+            var netMsg = NetMessage.Parser.ParseFrom(bytes);
+            int msgType = netMsg.Type;
+            //TODO:要做消息分发
+            if (msgType == (int)MessageNumber.C2S_RegisterUserInfoResquest)
+            {
+                var c2s_RegisterUserInfoRequest = C2S_RegisterUserInfoRequest.Parser.ParseFrom(netMsg.Content);
+                //给当前角色返回消息S2C_RegisterUserInfoResponse
+                var msg = new S2C_RegisterUserInfoResponse();
+                msg.Error = 0;
+                msg.Message = "";
+                msg.UserStateInfo = new CUserStateInfo()
+                {
+                    Rotate = new Vec3Data() { X = 0, Y = 0, Z = 0 },
+                    Pos = new Vec3Data() { X = 3.12f, Y = 4.17f, Z = 17.71f },
+                    UserInfo = new CUserInfo()
+                    {
+                        UserName = c2s_RegisterUserInfoRequest.UserInfo.UserName,
+                        UserId = socketIndex
+                    }
+                };
+                var returnMsg = new NetMessage()
+                {
+                    Type = (int)MessageNumber.S2C_RegisterUserInfoResponse,
+                    Content = msg.ToByteString(),
+                };
+                //foreach (var socket in allSockets)
+                //{
+                //    Console.WriteLine($"======给{socket.ConnectionInfo.Id}广播消息");
+                //    socket.Send(returnMsg.ToByteArray());
+                //}
+            }
+        }
+
+        /*
         public void BroacastBinaryMsg(byte[] bytes)
         {
             var netMsg = NetMessage.Parser.ParseFrom(bytes);
@@ -78,14 +98,6 @@ namespace ServerDemo
                     socket.Send(returnMsg.ToByteArray());
                 }
             }
-            //Console.WriteLine(netMsg.Type);
-            //var c2s_RegisterMsg = C2S_RegisterUserInfoRequest.Parser.ParseFrom(netMsg.Content);
-            //var user = c2s_RegisterMsg.UserInfo;
-            //foreach (var socket in allSockets)
-            //{
-            //    Console.WriteLine($"======给{socket.ConnectionInfo.Id}广播消息");
-            //    socket.Send(bytes);
-            //}
         }
 
         public void BroacastStringMsg(string message)
@@ -97,5 +109,6 @@ namespace ServerDemo
                 socket.Send(message);
             }
         }
+        */
     }
 }
---
+++ Server/src/Samples/ConsoleApp/Module/Session.cs
@@ -0,0 +1,113 @@
+﻿using Fleck;
+using Google.Protobuf;
+using Protoc;
+using System;
+
+namespace ServerDemo
+{
+    public class Session
+    {
+        private int sid = 0;
+        public int Sid { get { return sid; } }
+        private IWebSocketConnection socket;
+        private Guid connectInfoId;
+        private Action<Session> OnOpenEvent;
+        private Action<Session> OnCloseEvent;
+        public Session(int sid, IWebSocketConnection socket, Action<Session> onOpenEvent, Action<Session> onCloseEvent)
+        {
+            this.sid = sid;
+            this.socket = socket;
+            connectInfoId = socket.ConnectionInfo.Id;
+            socket.OnOpen += OnOpen;
+            socket.OnBinary += OnBinaryMsg;
+            socket.OnMessage += OnMessage;
+            socket.OnClose += OnClose;
+            socket.OnError += OnError;
+            OnOpenEvent = onOpenEvent;
+            OnCloseEvent = onCloseEvent;
+        }
+
+        public void Release()
+        {
+            socket.OnOpen -= OnOpen;
+            socket.OnBinary -= OnBinaryMsg;
+            socket.OnMessage -= OnMessage;
+            socket.OnClose -= OnClose;
+            socket.OnError -= OnError;
+
+            socket = null;
+        }
+
+        public void Send(byte[] msg)
+        {
+            socket.Send(msg);
+        }
+
+        public void Send(string msg)
+        {
+            socket.Send(msg);
+        }
+
+        void OnError(Exception ex)
+        {
+            Console.WriteLine(connectInfoId + ": " + ex.Message);
+        }
+
+        void OnClose()
+        {
+            Console.WriteLine(connectInfoId + ": Closed");
+            OnCloseEvent?.Invoke(this);
+        }
+
+        void OnOpen()
+        {
+            //allSockets.Add(socket);
+            //Console.WriteLine(id + ": Connected");
+            //socketIndex++;
+            //socketIdMap.Add(socket, socketIndex);
+            //idSocketMap.Add(socketIndex, socket);
+            Console.WriteLine(connectInfoId + ": Connected");
+            OnOpenEvent?.Invoke(this);
+        }
+
+        void OnMessage(string msg)
+        {
+
+        }
+
+        void OnBinaryMsg(byte[] bytes)
+        {
+            var netMsg = NetMessage.Parser.ParseFrom(bytes);
+            int msgType = netMsg.Type;
+            //TODO:要做消息分发
+            if (msgType == (int)MessageNumber.C2S_RegisterUserInfoResquest)
+            {
+                var c2s_RegisterUserInfoRequest = C2S_RegisterUserInfoRequest.Parser.ParseFrom(netMsg.Content);
+                //给当前角色返回消息S2C_RegisterUserInfoResponse
+                var msg = new S2C_RegisterUserInfoResponse();
+                msg.Error = 0;
+                msg.Message = "";
+                msg.UserStateInfo = new CUserStateInfo()
+                {
+                    Rotate = new Vec3Data() { X = 0, Y = 0, Z = 0 },
+                    Pos = new Vec3Data() { X = 3.12f, Y = 4.17f, Z = 17.71f },
+                    UserInfo = new CUserInfo()
+                    {
+                        UserName = c2s_RegisterUserInfoRequest.UserInfo.UserName,
+                        UserId = sid
+                    }
+                };
+                var returnMsg = new NetMessage()
+                {
+                    Type = (int)MessageNumber.S2C_RegisterUserInfoResponse,
+                    Content = msg.ToByteString(),
+                };
+                //foreach (var socket in allSockets)
+                //{
+                //    Console.WriteLine($"======给{socket.ConnectionInfo.Id}广播消息");
+                //    socket.Send(returnMsg.ToByteArray());
+                //}
+            }
+        }
+    }
+}
