96e68883817001b7bdb3353a7507455535afd567
dingxiaowei <1213250243@qq.com>
2023-03-01 20:42:50 +0800

    添加服务器执行目录

---
+++ Assets/Editor/Tools.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.IO;
+
+public class Tools : Editor
+{
+    [MenuItem("Tools/StartServer")]
+    public static void StartServer()
+    {
+        var unityProjPath = $"{Application.dataPath}";
+        var dir = Directory.CreateDirectory(unityProjPath);
+        var parentDir = dir.Parent;
+        System.Diagnostics.Process process = new System.Diagnostics.Process();
+        System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+        startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+        startInfo.FileName = $"{parentDir}/Server/src/Samples/ConsoleApp/bin/Debug/Fleck.Samples.ConsoleApp.exe";
+        process.StartInfo = startInfo;
+        process.Start();
+    }
+}
